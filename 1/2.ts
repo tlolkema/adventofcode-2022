48790c85c53dc7a73bc612eefd38dd84abb938a9
Tim Lolkema <timlolkema@gmail.com>
2022-12-05 23:56:46 +0100

    day 1

---
+++ 1/2.ts
@@ -0,0 +1,27 @@
+import { readFileSync } from 'fs'
+
+const file = readFileSync('./1/data.txt', 'utf-8')
+
+const getElves = (file: string): number[][] => {
+  return file
+    .trim()
+    .split('\n\n')
+    .map((str) => str.split('\n').map(Number))
+}
+
+const elvesTotals = (elves: number[][]): number[] => {
+  return elves.map((elf) => elf.reduce((a, b) => a + b))
+}
+
+const elvesSorted = (elvesTotals: number[]): number[] => {
+  return elvesTotals.sort((a, b) => b - a)
+}
+
+const elves = getElves(file)
+const totals = elvesTotals(elves)
+const sorted = elvesSorted(totals)
+
+const topThree = sorted.slice(0, 3)
+const sum = topThree.reduce((a, b) => a + b)
+
+console.log(sum)
