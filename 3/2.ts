1118dcdc29cc89234e0d48da095aae4d77a4b00d
Tim Lolkema <timlolkema@gmail.com>
2022-12-11 13:45:44 +0100

    day 3 - part 2

--- 3/2.ts
+++ 3/2.ts
@@ -1,4 +1,5 @@
 import { readFileSync } from 'fs'
+import { Priorities } from './priorities'
 
 const file = readFileSync(`${__dirname}/data.txt`, 'utf-8')
 
@@ -7,3 +8,21 @@ const getRucksacks = (file: string): string[] => {
 }
 
 const rucksacks = getRucksacks(file)
+
+let score = 0
+
+for (let i = 0; i < rucksacks.length; i += 3) {
+  const rucksackGroup = rucksacks.slice(i, i + 3)
+
+  const itemInFirstTwoRucksacks = rucksackGroup[0].split('').filter((char) => {
+    return rucksackGroup[1].includes(char)
+  })
+
+  const commonCharacter = itemInFirstTwoRucksacks.find((char) => {
+    return rucksackGroup[2].includes(char)
+  })
+
+  score += Priorities[commonCharacter]
+}
+
+console.log(score)
